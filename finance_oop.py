ca262c1a4c6131a8372aad00abfd6713df0e568e
Enzo Souza <enzo.florentinosouza.ismart@gmail.com>
2025-01-31 18:00:16 -0300

    classe para ativo/acao

---
+++ finance_oop.py
@@ -0,0 +1,32 @@
+class Ativo():
+    def __init__(self, nome, preco_medio, qtd, dy):
+        self.nome = nome
+        self.preco_medio = preco_medio
+        self.qtd = qtd
+        self.dy = dy #% ao ano
+        
+        self.ganho = 0
+        self.dividendos = 0
+    
+    #calcular ganho considerando todas as acoes
+    def calcGanho(self, preco_atual):
+        self.ganho = (preco_atual - self.preco_medio) * self.qtd
+        self.ganho = round(self.ganho, 2)
+        return self.ganho
+    
+    #calcular estimativa de dividendos anual
+    def calcDividendos(self, preco_atual):
+        self.dividendos = (preco_atual * (self.dy/100) * self.qtd)
+        self.dividendos = round(self.dividendos, 2)
+        return self.dividendos
+
+
+meu_ativo = Ativo("ABC3", 20, 2, 12)
+
+print(meu_ativo.ganho)
+meu_ativo.calcGanho(22)
+print(meu_ativo.ganho)
+
+print(meu_ativo.dividendos)
+meu_ativo.calcDividendos(22)
+print(meu_ativo.dividendos)
