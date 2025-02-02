d9092217b79abbac7dbfc9c5f253694ec326c1a3
Enzo Souza <enzo.florentinosouza.ismart@gmail.com>
2025-02-02 14:38:31 -0300

    funcao pra somar

--- renda.py
+++ renda.py
@@ -13,6 +13,13 @@ def criarFonteRenda(nome, valor):
 
     return fonte_renda
 
+def somar(lista):
+    soma = 0
+    for i in range(len(lista)):
+        soma += lista[i]["valor"]
+    
+    return soma
+
 #lista contendo dicionarios, cada dicionario é uma despesa
 lista_despesas = list() 
 
@@ -23,15 +30,23 @@ lista_fontes_renda = list()
 n_despesas = int(input())
 n_fontes_renda = int(input())
 
+#preenchendo lista_despesas
 for i in range(n_despesas):
     nome = input()
     valor = float(input())
     lista_despesas.append(criarDespesa(nome, valor))
 
+#preenchendo lista_fontes_renda
 for i in range(n_fontes_renda):
     nome = input()
     valor = float(input())
     lista_fontes_renda.append(criarFonteRenda(nome, valor))
 
+total_despesas = somar(lista_despesas)
+total_fontes_renda = somar(lista_fontes_renda)
+
 print(lista_despesas)
+print(total_despesas)
+
 print(lista_fontes_renda)
+print(total_fontes_renda)
