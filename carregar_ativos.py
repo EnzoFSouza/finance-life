57261301bbb522f744f9cff6c50a7c4e4e8528ec
Enzo Souza <enzo.florentinosouza.ismart@gmail.com>
2025-02-05 10:43:37 -0300

    otimizacoes no carregar_ativos

--- carregar_ativos.py
+++ carregar_ativos.py
@@ -1,19 +1,19 @@
 import json
 from finance_oop import Ativo
 
+def printarOpcoes():
+    for i in range(len(lista_de_objetos)):
+        print(lista_de_objetos[i].nome, end = " ")
+    print()
+
 def encontrarAtivo(nome):
     #Percorrendo todos os ativos até encontrar aquele com o nome digitado
     for i in range(len(lista_de_objetos)):
         if lista_de_objetos[i].nome == nome:
             return i
+    return None
 
-def printarOpcoes():
-    for i in range(len(lista_de_objetos)):
-        print(lista_de_objetos[i].nome, end = " ")
-
-precos_atuais = list()
-
-#Somar precos atuais, multiplicar pelas respectivas quantidades e obter o valor da carteira
+#Somatoria da multiplicacao dos precos atuais pelas respectivas quantidades para obter o valor da carteira
 def obterValorCarteira():
     valor_carteira = 0
     for i in range(len(precos_atuais)):
@@ -26,48 +26,47 @@ def status(objeto, preco_atual, valor_carteira):
     objeto.calcParticipacao(preco_atual, valor_carteira)
     objeto.calcValorTotalAtivo(preco_atual)
 
-    print("Nome: ", objeto.nome)
-    print("Preco Medio: ", objeto.preco_medio)
-    print("Preco Atual: ", preco_atual)
-    print("Quantidade: ", objeto.qtd)
-    print("DY: ", objeto.dy)
-    print("Ganho: ", objeto.ganho)
-    print("Dividendos: ", objeto.dividendos)
+    print(f"Nome: {objeto.nome}")
+    print(f"Preco Medio: {objeto.preco_medio}")
+    print(f"Preco Atual: {preco_atual}")
+    print(f"Quantidade: {objeto.qtd}")
+    print(f"DY: {objeto.dy}")
+    print(f"Ganho: {objeto.ganho}")
+    print(f"Dividendos: {objeto.dividendos}")
     print(f"Participacao: {objeto.participacao}%")
     print(f"Valor total do ativo: {objeto.valor_total}")
 
 #criando uma lista de objetos
 lista_de_objetos = list()
 
+#lista de precos atuais
+precos_atuais = list()
+
 #pegando todos os ativos salvos em "ativos.txt" e obtendo uma lista de dicionários
 with open("finance-life/ativos.txt", "r") as f:
-    ativos = json.load(f) #lista de dicionarios
-    #print(ativos)
+    #lista de dicionarios
+    ativos = json.load(f) 
     f.close()
 
 for i in range(len(ativos)):
-    #print(ativos[i])
     #preenchendo a lista de objetos (com objetos)
     meu_ativo = Ativo(ativos[i]["nome"], ativos[i]["preco_medio"], ativos[i]["quantidade"], ativos[i]["dy"])
     lista_de_objetos.append(meu_ativo)
+
     #preenchendo a lista de precos atuais com o valor do preco medio + 5 para fins de testes
     precos_atuais.append(ativos[i]["preco_medio"] + 5)
 
-print(precos_atuais)
-#printando o nome de todos os objetos em "lista_de_objetos"
-#for i in range(len(lista_de_objetos)):
-#    print(lista_de_objetos[i].nome)
-
+valor_carteira = obterValorCarteira()
 printarOpcoes()
-print()
+
 nome = input()
 while (nome != "sair"):
     indice = encontrarAtivo(nome)
-    #print(lista_de_objetos[indice].nome)
-    #print(lista_de_objetos[indice].preco_medio)
-    #print(lista_de_objetos[indice].qtd)
-    #print(lista_de_objetos[indice].dy)
-    status(lista_de_objetos[indice], precos_atuais[indice], obterValorCarteira())
+    if (indice == None):
+        print("Ativo não encontrado, digite novamente")
+    
+    else:
+        status(lista_de_objetos[indice], precos_atuais[indice], valor_carteira)
+    
     printarOpcoes()
-    print()
     nome = input()
