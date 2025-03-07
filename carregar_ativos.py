fe010e25b5777f8e4d6e3b51524c4d29f28b6a68
Enzo Souza <enzo.florentinosouza.ismart@gmail.com>
2025-03-07 15:51:59 -0300

    criar classe cliente/pessoa

--- carregar_ativos.py
+++ carregar_ativos.py
@@ -1,66 +1,8 @@
 import json
 from finance_oop import Ativo
+from cliente_oop import Pessoa
 
-GANHO_DIA = 1.0
-
-def calcGanhoMes():
-    x = GANHO_DIA * 22
-    #print(f"Recebendo {GANHO_DIA} por dia durante um mês, você receberá R${x}")
-    return x
-
-def calcGanhoAno(num_anos):
-    valor = calcGanhoMes() * 12 * num_anos 
-    print(f"Em {num_anos} ano(s), você receberá R${valor:.2f}")
-    return valor
-
-def calcTempoMeta(meta):
-    tempo = meta // calcGanhoAno(1) #tempo em anos
-    return tempo
-
-def printarOpcoes():
-    for i in range(len(lista_de_objetos)):
-        print(lista_de_objetos[i].nome, end = " ")
-    
-    nomes_funcoes = ["calcGanhoMes", "calcGanhoAno", "calcTempoMeta"]
-    for i in range(len(nomes_funcoes)):
-        print(nomes_funcoes[i], end = " ")
-        
-    print()
-
-def encontrarAtivo(nome):
-    #Percorrendo todos os ativos até encontrar aquele com o nome digitado
-    for i in range(len(lista_de_objetos)):
-        if lista_de_objetos[i].nome == nome:
-            return i
-    return None
-
-#Somatoria da multiplicacao dos precos atuais pelas respectivas quantidades para obter o valor da carteira
-def obterValorCarteira():
-    valor_carteira = 0
-    for i in range(len(precos_atuais)):
-        valor_carteira += precos_atuais[i] * lista_de_objetos[i].qtd
-    return valor_carteira
-
-def status(objeto, preco_atual, valor_carteira):
-    objeto.calcGanho(preco_atual)
-    objeto.calcDividendosAnual(preco_atual)
-    objeto.calcParticipacao(preco_atual, valor_carteira)
-    objeto.calcValorTotalAtivo(preco_atual)
-    objeto.calcBolaDeNeve(preco_atual)
-    objeto.calcRendimentoMes(preco_atual)
-
-    print(f"Nome: {objeto.nome}")
-    print(f"Preco Medio: R${objeto.preco_medio:.2f}")
-    print(f"Preco Atual: R${preco_atual:.2f}")
-    print(f"Quantidade: {objeto.qtd}")
-    print(f"DY Anual: {objeto.dy_anual}")
-    print(f"DY Mensal: {objeto.dy_mensal}")
-    print(f"Ganho: R${objeto.ganho:.2f}")
-    print(f"Rendimento estimado para o ano: R${objeto.dividendos:.2f}")
-    print(f"Participacao: {objeto.participacao}%")
-    print(f"Valor total do ativo: R${objeto.valor_total:.2f}")
-    print(f"Quantidade para efeito bola de neve: {objeto.qtd_bola_neve}")
-    print(f"Rendimento estimado para o mês: R${objeto.expectativa_de_ganho:.2f}")
+GANHO_DIARIO = 1.5
 
 #criando uma lista de objetos
 lista_de_objetos = list()
@@ -82,31 +24,5 @@ for i in range(len(ativos)):
     #preenchendo a lista de precos atuais com o valor do preco medio + 5 para fins de testes
     precos_atuais.append(ativos[i]["preco_medio"] + 5)
 
-valor_carteira = obterValorCarteira()
-printarOpcoes()
-
-nome = input()
-while (nome != "sair"):
-    if nome == "calcGanhoMes":
-        ganho_mensal = calcGanhoMes()
-        print(f"Recebendo {GANHO_DIA} por dia durante um mês, você receberá R${ganho_mensal}")
-    
-    elif nome == "calcGanhoAno":
-        n_anos = int(input())
-        ganho_anual = calcGanhoAno(n_anos)
-    
-    elif nome == "calcTempoMeta":
-        meta = int(input())
-        tempo_anos = calcTempoMeta(meta)
-        print(f"Em {tempo_anos}, você atingirá sua meta de {meta}")
-
-    else:
-        indice = encontrarAtivo(nome)
-        if (indice == None):
-            print("Ativo não encontrado, digite novamente")
-    
-        else:
-            status(lista_de_objetos[indice], precos_atuais[indice], valor_carteira)
-    
-    printarOpcoes()
-    nome = input()
+pessoa = Pessoa(lista_de_objetos, precos_atuais, GANHO_DIARIO)
+pessoa.loop()
---
+++ cliente_oop.py
@@ -0,0 +1,99 @@
+class Pessoa():
+    def __init__(self, carteira, precos_atuais, ganho_diario):
+        self.carteira = carteira #lista de objetos
+        self.precos_atuais = precos_atuais #lista com precos atuais dos ativos na ordem correta
+        self.valor_carteira = 0
+        self.ganho_diario = ganho_diario
+        self.ganho_mes = 0
+        self.ganho_ano = 0
+        self.num_anos = 1
+        self.meta = 0
+        self.tempo_meta = 0 #Em anos
+        self.nome = ""
+        self.indice = None
+    
+    def calcGanhoMes(self):
+        self.ganho_mes = self.ganho_diario * 22
+        return self.ganho_mes
+
+    def calcGanhoAno(self):
+        self.ganho_ano = self.calcGanhoMes() * 12 * self.num_anos 
+        print(f"Em {self.num_anos} ano(s), você receberá R${self.ganho_ano:.2f}")
+        return self.ganho_ano
+
+    def calcTempoMeta(self):
+        self.tempo_meta = self.meta // self.calcGanhoAno() #tempo em anos
+        return self.tempo_meta
+
+    def status(self, objeto, preco_atual):
+        objeto.calcGanho(preco_atual)
+        objeto.calcDividendosAnual(preco_atual)
+        objeto.calcParticipacao(preco_atual, self.valor_carteira)
+        objeto.calcValorTotalAtivo(preco_atual)
+        objeto.calcBolaDeNeve(preco_atual)
+        objeto.calcRendimentoMes(preco_atual)
+
+        print(f"Nome: {objeto.nome}")
+        print(f"Preco Medio: R${objeto.preco_medio:.2f}")
+        print(f"Preco Atual: R${preco_atual:.2f}")
+        print(f"Quantidade: {objeto.qtd}")
+        print(f"DY Anual: {objeto.dy_anual}")
+        print(f"DY Mensal: {objeto.dy_mensal}")
+        print(f"Ganho: R${objeto.ganho:.2f}")
+        print(f"Rendimento estimado para o ano: R${objeto.dividendos:.2f}")
+        print(f"Participacao: {objeto.participacao}%")
+        print(f"Valor total do ativo: R${objeto.valor_total:.2f}")
+        print(f"Quantidade para efeito bola de neve: {objeto.qtd_bola_neve}")
+        print(f"Rendimento estimado para o mês: R${objeto.expectativa_de_ganho:.2f}")
+
+    def encontrarAtivo(self):
+        #Percorrendo todos os ativos até encontrar aquele com o nome digitado
+        for i in range(len(self.carteira)):
+            if self.carteira[i].nome == self.nome:
+                return i
+        return None
+
+    def obterValorCarteira(self):
+        for i in range(len(self.precos_atuais)):
+            self.valor_carteira += self.precos_atuais[i] * self.carteira[i].qtd
+        return self.valor_carteira
+
+    def opcoes(self):
+        for i in range(len(self.carteira)):
+            print(self.carteira[i].nome, end = " ")
+    
+        nomes_funcoes = ["calcGanhoMes", "calcGanhoAno", "calcTempoMeta"]
+        for i in range(len(nomes_funcoes)):
+            print(nomes_funcoes[i], end = " ")
+        
+        print()
+
+    def loop(self):
+        self.opcoes()
+        self.nome = input()
+        while (self.nome != "sair"):
+            if self.nome == "calcGanhoMes":
+                self.ganho_mensal = self.calcGanhoMes()
+                print(f"Recebendo {self.ganho_diario} por dia durante um mês, você receberá R${self.ganho_mensal}")
+        
+            elif self.nome == "calcGanhoAno":
+                self.num_anos = int(input())
+                self.ganho_anual = self.calcGanhoAno()
+                self.num_anos = 1
+    
+            elif self.nome == "calcTempoMeta":
+                self.meta = int(input())
+                self.tempo_meta = self.calcTempoMeta()
+                print(f"Em {self.tempo_meta}, você atingirá sua meta de {self.meta}")
+
+            else:
+                self.indice = self.encontrarAtivo()
+                if (self.indice == None):
+                    print("Ativo não encontrado, digite novamente")
+    
+                else:
+                    self.valor_carteira = self.obterValorCarteira()
+                    self.status(self.carteira[self.indice], self.precos_atuais[self.indice])
+    
+            self.opcoes()
+            self.nome = input()
