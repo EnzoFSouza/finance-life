f463b724884aad46cb77c28ae6bc7c440b1eb9da
Enzo Souza <enzo.florentinosouza.ismart@gmail.com>
2025-03-09 14:56:26 -0300

    obter shape de um dataframe

--- carregar_ativos_2.py
+++ carregar_ativos_2.py
@@ -4,8 +4,10 @@ ativos_df = pd.read_csv("finance-life/ativos.csv")
 valores_df = ativos_df[["Preco Medio", "Dividend Yield", "Quantidade", "Preco Atual"]]
 coluna_preco_medio = ativos_df[["Preco Medio"]]
 preco_especifico = ativos_df.at[0, "Preco Medio"]
-
 print(ativos_df) #print dataframe inteiro
 print(valores_df) #print das colunas preco medio, dividend yield, quantidade e preco atual
 print(coluna_preco_medio) #print coluna preco medio
 print(preco_especifico) #print valor especifico da linha 0 e coluna preco medio
+print(ativos_df.shape)
+print(ativos_df.shape[0]) #linhas
+print(ativos_df.shape[1]) #colunas
