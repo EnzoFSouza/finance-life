d9931ce73f76b3b7c3c6c9aa303d8bb6d94af7de
Enzo Souza <enzo.florentinosouza.ismart@gmail.com>
2025-03-08 21:54:17 -0300

    "at" comando para obter um unico valor num dataframe. Sera util no futuro

--- carregar_ativos_2.py
+++ carregar_ativos_2.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
-ativos_df = pd.read_csv("ativos.csv")
+ativos_df = pd.read_csv("finance-life/ativos.csv")
 valores_df = ativos_df[["Preco Medio", "Dividend Yield", "Quantidade", "Preco Atual"]]
-print(ativos_df)
-print(valores_df)
+coluna_preco_medio = ativos_df[["Preco Medio"]]
+preco_especifico = ativos_df.at[0, "Preco Medio"]
+
+print(ativos_df) #print dataframe inteiro
+print(valores_df) #print das colunas preco medio, dividend yield, quantidade e preco atual
+print(coluna_preco_medio) #print coluna preco medio
+print(preco_especifico) #print valor especifico da linha 0 e coluna preco medio
